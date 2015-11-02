971cc3f8db792555bd95876a8fb6e3ff201eafb4
--replace-all <diaconu.codrin@gmail.com>
2015-11-02 15:47:07 -0500

    Refactor API with underscore

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -25,12 +26,8 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
    var id = Number(req.params.id); //We used Number() here because req.params return a string. We could have also set our id to string and leave the req.params as is.
-    var found;
-    todos.forEach(function (todo) {
-        if(todo.id === id){
-            found = todo;
-        }
-    });
+    var found = _.findWhere(todos, {id: id});
+
     if(found){
         res.json(found);
     } else {
@@ -40,7 +37,15 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    //We are filtering out any other data except for "description" and "completed"
+    var body = _.pick(req.body, 'description', 'completed');
+    //More filtering
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+    //Getting rid of unnecessary spaces
+    body.description = body.description.trim();
+
     body.id = todoNextId;
     todoNextId++;
     todos.push(body);
