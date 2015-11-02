4a602ecdf1c176d3d688c83406145844d0b479d0
--replace-all <diaconu.codrin@gmail.com>
2015-11-02 15:00:30 -0500

    Add POST for todos

--- server.js
+++ server.js
@@ -2,35 +2,27 @@
  * Created by Codrin on 2015-11-02.
  */
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-    description: 'Meet mom for lunch',
-    completed: false,
-    id: 1
-}, {
-    description: 'Walk the dog',
-    completed: false,
-    id: 2
-}, {
-    description: 'Go to market',
-    completed: true,
-    id: 3
-}];
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
    res.send('Todo API root');
 });
 
-//Get /todos
+//GET /todos
 app.get('/todos', function (req, res) {
    //res.send(todos); This will not work since we can only send JSON;
    //We need to use .json
     res.json(todos);
 });
 
-//Get /todos/:id
+//GET /todos/:id
 app.get('/todos/:id', function (req, res) {
    var id = Number(req.params.id); //We used Number() here because req.params return a string. We could have also set our id to string and leave the req.params as is.
     var found;
@@ -44,6 +36,15 @@ app.get('/todos/:id', function (req, res) {
     } else {
         res.status(404).send();
     }
+});
+
+//POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    body.id = todoNextId;
+    todoNextId++;
+    todos.push(body);
+    res.json(body);
 });
 
 app.listen(PORT, function () {
